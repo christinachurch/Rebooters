4645b2c148de0b54bfb33dc3e322b6cb1a5bade3
jbishop122 <130241759+jbishop122@users.noreply.github.com>
2023-04-23 15:26:30 -0500

    Add files via upload

--- Recreation Center Application/recreation center application.py
+++ Recreation Center Application/recreation center application.py
@@ -4,24 +4,24 @@ from tkinter import messagebox
 from datetime import datetime
 
 landEquipment = {
-'2 Person Tent' : 15,
-'4 Person Tent' : 20,
-'6 Person Tent' : 25,
-'8 Person Tent' : 30,
-'Sleeping Bag': 8,
-'Sleeping Pad': 5,
-'Backpack': 10,
-'Medium Stuff Sack': 3,
-'Large Stuff Sack': 5,
-'2 Burner Stove': 10,
-'Cook Pot Set': 5,
-'Lantern': 5,
-'Cooler': 5,
-'Ice Chest': 5,
-'Canopy Tent': 20,
-'Crazy Creek Chair': 5,
-'Single Hammock': 8,
-'Double Hammock': 10,
+    '2 Person Tent' : 15,
+    '4 Person Tent' : 20,
+    '6 Person Tent' : 25,
+    '8 Person Tent' : 30,
+    'Sleeping Bag': 8,
+    'Sleeping Pad': 5,
+    'Backpack': 10,
+    'Medium Stuff Sack': 3,
+    'Large Stuff Sack': 5,
+    '2 Burner Stove': 10,
+    'Cook Pot Set': 5,
+    'Lantern': 5,
+    'Cooler': 5,
+    'Ice Chest': 5,
+    'Canopy Tent': 20,
+    'Crazy Creek Chair': 5,
+    'Single Hammock': 8,
+    'Double Hammock': 10,
 }
 
 waterEquipment = {
@@ -41,38 +41,46 @@ class DateRangeSelector:
     def __init__(self, master):
         self.master = master
         self.master.title("Date Range Selector")
-        self.master.geometry("300x250")
-        self.master.configure(bg="#367653") 
+        self.master.geometry("800x500")
+        self.master.configure(bg="#367653")  # Set background color
+
+        # Add Frame with background color
+        self.frame = ttk.Frame(self.master, padding=(10, 10, 10, 0), style='my.TFrame')
+        self.frame.pack(fill=tk.BOTH, expand=True)
 
         # Start Date Label
-        self.start_label = ttk.Label(self.master, text="Start Date:")
+        self.start_label = ttk.Label(self.frame, text="Start Date:")
         self.start_label.pack(pady=5)
 
         # Start Date Entry
-        self.start_entry = ttk.Entry(self.master)
+        self.start_entry = ttk.Entry(self.frame)
         self.start_entry.pack(pady=5)
 
         # End Date Label
-        self.end_label = ttk.Label(self.master, text="End Date:")
+        self.end_label = ttk.Label(self.frame, text="End Date:")
         self.end_label.pack(pady=5)
 
         # End Date Entry
-        self.end_entry = ttk.Entry(self.master)
+        self.end_entry = ttk.Entry(self.frame)
         self.end_entry.pack(pady=5)
 
         # Land Rental Checkbox
         self.land_var = tk.BooleanVar()
-        self.land_checkbox = ttk.Checkbutton(self.master, text="Land Rental", variable=self.land_var)
+        self.land_checkbox = ttk.Checkbutton(self.frame, text="Land Rental", variable=self.land_var)
         self.land_checkbox.pack(pady=5)
 
         # Water Rental Checkbox
         self.water_var = tk.BooleanVar()
-        self.water_checkbox = ttk.Checkbutton(self.master, text="Water Rental", variable=self.water_var)
+        self.water_checkbox = ttk.Checkbutton(self.frame, text="Water Rental", variable=self.water_var)
         self.water_checkbox.pack(pady=5)
 
         # Submit Button
-        self.submit_button = ttk.Button(self.master, text="Submit", command=self.submit)
+        self.submit_button = ttk.Button(self.frame, text="Submit", command=self.submit)
         self.submit_button.pack(pady=10)
+
+        # Style the frame
+        style = ttk.Style()
+        style.configure('my.TFrame', background='#367653')  # Set background color of frame
 
     def submit(self):
         start_date = self.start_entry.get()
@@ -84,20 +92,106 @@ class DateRangeSelector:
         try:
             start_date = datetime.strptime(start_date, "%Y-%m-%d")
             end_date = datetime.strptime(end_date, "%Y-%m-%d")
+
+            # Create a new page in a notebook
+            new_page = ttk.Frame(self.master)
+            new_page.pack(fill=tk.BOTH, expand=True)
+
+            # Add a label to the new page
+
+            if land_rental == True:
+                label = ttk.Label(new_page, text="Land Rental")
+                label.pack(pady=10)
+
+                # Tent Label
+                self.tent_label = ttk.Label(self.master, text="Tent:")
+                self.tent_label.pack(pady=5)
+
+                # Tent Dropdown Box
+                self.tent_var = tk.StringVar()
+                self.tent_dropdown = ttk.Combobox(self.master, values=[str(i) for i in range(10)], state="readonly", textvariable=self.tent_var)
+                self.tent_dropdown.current(0)
+                self.tent_dropdown.pack(pady=5)
+
+                # Tent Output Box
+                self.tent_output = ttk.Label(self.master, text="")
+                self.tent_output.pack(pady=5)
+
+                # Sleeping Bag Label
+                self.sleeping_bag_label = ttk.Label(self.master, text="Sleeping Bag:")
+                self.sleeping_bag_label.pack(pady=5)
+
+                # Sleeping Bag Dropdown Box
+                self.sleeping_bag_var = tk.StringVar()
+                self.sleeping_bag_dropdown = ttk.Combobox(self.master, values=[str(i) for i in range(10)], state="readonly", textvariable=self.sleeping_bag_var)
+                self.sleeping_bag_dropdown.current(0)
+                self.sleeping_bag_dropdown.pack(pady=5)
+
+                # Sleeping Bag Output Box
+                self.sleeping_bag_output = ttk.Label(self.master, text="")
+                self.sleeping_bag_output.pack(pady=5)
+
+                # Backpack Label
+                self.backpack_label = ttk.Label(self.master, text="Backpack:")
+                self.backpack_label.pack(pady=5)
+
+                # Backpack Dropdown Box
+                self.backpack_var = tk.StringVar()
+                self.backpack_dropdown = ttk.Combobox(self.master, values=[str(i) for i in range(10)], state="readonly", textvariable=self.backpack_var)
+                self.backpack_dropdown.current(0)
+                self.backpack_dropdown.pack(pady=5)
+
+                # Backpack Output Box
+                self.backpack_output = ttk.Label(self.master, text="")
+                self.backpack_output.pack(pady=5)
+
+                # Accessories Label
+                self.accessories_label = ttk.Label(self.master, text="Accessories:")
+                self.accessories_label.pack(pady=5)
+
+                # 2 Burner Stove Label
+                self.stove_label = ttk.Label(self.master, text="2 Burner Stove:")
+                self.stove_label.pack(pady=5)
+
+                # 2 Burner Stove Dropdown Box
+                self.stove_var = tk.StringVar()
+                self.stove_dropdown = ttk.Combobox(self.master, values=[str(i) for i in range(10)], state="readonly", textvariable=self.stove_var)
+                self.stove_dropdown.current(0)
+                self.stove_dropdown.pack(pady=5)
+
+                # 2 Burner Stove Output Box
+                self.stove_output = ttk.Label(self.master, text="")
+                self.stove_output.pack(pady=5)
+
+                # Cook Pot Set Label
+                self.cook_pot_label = ttk.Label(self.master, text="Cook Pot Set:")
+                self.cook_pot_label.pack(pady=5)
+
+                # Cook Pot Set Dropdown Box
+                self.cook_pot_var = tk.StringVar()
+                self.cook_pot_dropdown = ttk.Combobox(self.master, values=[str(i) for i in range(10)], state="readonly", textvariable=self.cook_pot)
+
+
+            elif water_rental == True & land_rental == False:
+                label = ttk.Label(new_page, text="Water Rental")
+                label.pack(pady=10)
+
+            # Add a button to go back to the main page
+            back_button = ttk.Button(new_page, text="Back", command=lambda: self.show_page(self.frame))
+            back_button.pack()
+
+            # Show the new page and hide the main page
+            self.show_page(new_page)
+            self.hide_page(self.frame)
+
         except ValueError:
-            messagebox.showerror("Error", "Invalid date format. Please use YYYY-MM-DD.")
-            return
+            messagebox.showerror("Error", "Invalid date format. Please enter dates in the format: YYYY-MM-DD")
 
-        # Check if start date is before end date
-        if start_date > end_date:
-            messagebox.showerror("Error", "Start date cannot be after end date.")
-            return
+    def show_page(self, page):
+        page.tkraise()
 
-        # Perform desired action with start_date, end_date, land_rental, and water_rental
-        print("Start Date:", start_date)
-        print("End Date:", end_date)
-        print("Land Rental:", land_rental)
-        print("Water Rental:", water_rental)
+    def hide_page(self, page):
+        page.pack_forget()
 
 if __name__ == '__main__':
     root = tk.Tk()
