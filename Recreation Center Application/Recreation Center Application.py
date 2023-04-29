e4d84fbc539755394cc5320c03e0054847b4f0e3
jbishop122 <130241759+jbishop122@users.noreply.github.com>
2023-04-29 16:49:36 -0500

    Add files via upload

--- Recreation Center Application/Recreation Center Application.py
+++ Recreation Center Application/Recreation Center Application.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageTk
 import tkinter as tk
 from tkinter import ttk
+import datetime
 
 
 # Dictionaries
@@ -33,7 +34,7 @@ class RecreationCenterApplication:
     def __init__(self, master):
         self.master = master
         self.master.title("Recreation Center Equipment Rental")
-        self.master.geometry("800x500")
+        self.master.geometry("439x400")
         self.master.configure(bg="#367653")  # Set background color
 
         self.pagenum = 1
@@ -60,149 +61,163 @@ class RecreationCenterApplication:
                 hammockCost = hammock * landEquipment[i]
         totalLandCost = tentCost + sleepingbagCost + backpackCost + twoburnerstoveCost + lanternCost + cookpotsetCost + watercoolerCost + hammockCost
         print(totalLandCost)
+        return tentCost, sleepingbagCost, backpackCost, twoburnerstoveCost, lanternCost, cookpotsetCost, watercoolerCost, hammockCost
 
 
     def dateSelection(self):
-        page = tk.Frame(self.master)
+        page = tk.Frame(self.master, width=300, height=600)
+
+        
         page.grid()
-        tk.Label(page, text='Date Selection').grid(row=0)
-        tk.Button(page, text='Next Page', command=self.goto_page2).grid(row=5)
 
-        tk.Label(page, text="Start Date:").grid(row=2, column=0, sticky=tk.W)
-        tk.Entry(page, width=10).grid(row=2, column=1, sticky=tk.W)
+        tk.Label(page, text='Date Selection', font=('Arial', 16)).grid(row=0,pady= 10)
 
-        tk.Label(page, text="End Date:").grid(row=3, column=0, sticky=tk.W)
-        tk.Entry(page, width=10).grid(row=3, column=1, sticky=tk.W)
+        tk.Label(page, text="Start Date:", font=('Arial', 14)).grid(row=2, column=0, pady= 10, sticky=tk.W)
+        self.startDate = tk.Entry(page, width=15).grid(row=2, column=1, sticky=tk.W)
+
+        '''if startDate < datetime.today():
+            tk.Label(page, text="Please enter a valid date", font=('Arial', 14)).grid(row=2, column=2, sticky=tk.W)'''
+
+        tk.Label(page, text="End Date:", font=('Arial', 14)).grid(row=3, column=0,pady= 10, sticky=tk.W)
+        self.endDate = tk.Entry(page, width=15).grid(row=3, column=1, pady= 10, sticky=tk.W)
 
         self.land_var = tk.BooleanVar()
-        ttk.Checkbutton(page, text="Land Rentals", variable=self.land_var).grid(row=4, column=0, sticky=tk.W)
+        ttk.Checkbutton(page, text="Land Rentals", variable=self.land_var).grid(row=7, column=0, pady= 20, sticky=tk.W)
         
         self.water_var = tk.BooleanVar()
-        ttk.Checkbutton(page, text="Water Rentals", variable=self.water_var).grid(row=4, column=1, sticky=tk.W)
-        
+        ttk.Checkbutton(page, text="Water Rentals", variable=self.water_var).grid(row=7, column=1, pady= 10, sticky=tk.W)
+        tk.Button(page, text='Next Page', command=self.goto_page2).grid(row=9, pady= 10)
 
     
 
 
     def landRental(self):
+        global landEquipment
         page = tk.Frame(self.master)
         page.grid()
-        tk.Label(page, text='Land Rentals').grid(row=0)
+        tk.Label(page, text='Land Rentals', font=('Arial', 14, 'bold')).grid(row=0)
         tk.Button(page, text='Back', command=self.goto_page1).grid(row=11)
         tk.Button(page, text='Next Page', command=self.goto_page3).grid(row=11, column=1)
         
 
         # Tent
-        tk.Label(page, text='Tent').grid(row=2, column=0, sticky=tk.W)
+        tk.Label(page, text='Tent', font=('Arial', 14)).grid(row=2, column=0, sticky=tk.W)
         self.tent_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.tent_var, values=[0,1,2,3,4,5]).grid(row=2, column=1, sticky=tk.W)
-        tk.Label(page, text=self.tent_var.get()).grid(row=2, column=2, sticky=tk.W)
+        tk.Label(page, text='$15').grid(row=2, column=2, sticky=tk.W)
 
         # Sleeping Bag
-        tk.Label(page, text='Sleeping Bag').grid(row=3, column=0, sticky=tk.W)
+        tk.Label(page, text='Sleeping Bag', font=('Arial', 14)).grid(row=3, column=0, sticky=tk.W)
         self.sleepingbag_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.sleepingbag_var, values=[0,1,2,3,4,5]).grid(row=3, column=1, sticky=tk.W)
-        tk.Label(page, text=self.sleepingbag_var.get()).grid(row=3, column=2, sticky=tk.W)
+        tk.Label(page, text='$8').grid(row=3, column=2, sticky=tk.W)
 
         # Backpack
-        tk.Label(page, text='Backpack').grid(row=4, column=0, sticky=tk.W)
+        tk.Label(page, text='Backpack', font=('Arial', 14)).grid(row=4, column=0, sticky=tk.W)
         self.backpack_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.backpack_var, values=[0,1,2,3,4,5]).grid(row=4, column=1, sticky=tk.W)
-        tk.Label(page, text=self.backpack_var.get()).grid(row=4, column=2, sticky=tk.W)
+        tk.Label(page, text='$10').grid(row=4, column=2, sticky=tk.W)
 
-        tk.Label(page, text='Accessories').grid(row=5, column=0, sticky=tk.W)
+        tk.Label(page, text='Accessories', font=('Arial', 14, 'bold')).grid(row=5, column=0, sticky=tk.W)
 
         # 2 Burner Stove
-        tk.Label(page, text='2 Burner Stove').grid(row=6, column=0, sticky=tk.W)
+        tk.Label(page, text='2 Burner Stove', font=('Arial', 14)).grid(row=6, column=0, sticky=tk.W)
         self.twoburnerstove_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.twoburnerstove_var, values=[0,1,2,3,4,5]).grid(row=6, column=1, sticky=tk.W)
-        tk.Label(page, text=self.twoburnerstove_var.get()).grid(row=6, column=2, sticky=tk.W)
+        tk.Label(page, text='$10').grid(row=6, column=2, sticky=tk.W)
 
         # Lantern
-        tk.Label(page, text='Lantern').grid(row=7, column=0, sticky=tk.W)
+        tk.Label(page, text='Lantern', font=('Arial', 14)).grid(row=7, column=0, sticky=tk.W)
         self.lantern_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.lantern_var, values=[0,1,2,3,4,5]).grid(row=7, column=1, sticky=tk.W)
-        tk.Label(page, text=self.lantern_var.get()).grid(row=7, column=2, sticky=tk.W)
+        tk.Label(page, text='$5').grid(row=7, column=2, sticky=tk.W)
 
         # Cook Pot Set
-        tk.Label(page, text='Cook Pot Set').grid(row=8, column=0, sticky=tk.W)
+        tk.Label(page, text='Cook Pot Set', font=('Arial', 14)).grid(row=8, column=0, sticky=tk.W)
         self.cookpotset_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.cookpotset_var, values=[0,1,2,3,4,5]).grid(row=8, column=1, sticky=tk.W)
-        tk.Label(page, text=self.cookpotset_var.get()).grid(row=8, column=2, sticky=tk.W)
+        tk.Label(page, text='$5').grid(row=8, column=2, sticky=tk.W)
 
         # Water Cooler
-        tk.Label(page, text='Water Cooler').grid(row=9, column=0, sticky=tk.W)
+        tk.Label(page, text='Water Cooler', font=('Arial', 14)).grid(row=9, column=0, sticky=tk.W)
         self.watercooler_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.watercooler_var, values=[0,1,2,3,4,5]).grid(row=9, column=1, sticky=tk.W)
-        tk.Label(page, text=self.watercooler_var.get()).grid(row=9, column=2, sticky=tk.W)
+        tk.Label(page, text='$5').grid(row=9, column=2, sticky=tk.W)
 
         # Hammock
-        tk.Label(page, text='Hammock').grid(row=10, column=0, sticky=tk.W)
+        tk.Label(page, text='Hammock', font=('Arial', 14)).grid(row=10, column=0, sticky=tk.W)
         self.hammock_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.hammock_var, values=[0,1,2,3,4,5]).grid(row=10, column=1, sticky=tk.W)
-        tk.Label(page, text=self.hammock_var.get()).grid(row=10, column=2, sticky=tk.W)
-        
+        tk.Label(page, text='$8').grid(row=10, column=2, sticky=tk.W)
+
+        self.calcTotal = lambda: self.calculateLandCost(self.tent_var.get(), self.sleepingbag_var.get(), self.backpack_var.get(), self.twoburnerstove_var.get(), self.lantern_var.get(), self.cookpotset_var.get(), self.watercooler_var.get(), self.hammock_var.get())
+        tk.Button(page, text='Calculate', command=self.calcTotal).grid(row=11, column=2)
+
+
 
     def waterRental(self):
         page = tk.Frame(self.master)
         page.grid()
-        tk.Label(page, text='Water Rentals').grid(row=0)
+        tk.Label(page, text='Water Rentals', font=('Arial', 14, 'bold')).grid(row=0)
         tk.Button(page, text='Back', command=self.goto_page1).grid(row=11)
         tk.Button(page, text='Next Page', command=self.goto_page3).grid(row=11, column=1)
 
         # Canoe
-        tk.Label(page, text='Canoe').grid(row=2, column=0, sticky=tk.W)
+        tk.Label(page, text='Canoe', font=('Arial', 14)).grid(row=2, column=0, sticky=tk.W)
         self.canoe_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.canoe_var, values=[0,1,2,3,4,5]).grid(row=2, column=1, sticky=tk.W)
-        tk.Label(page, text=self.canoe_var.get()).grid(row=2, column=2, sticky=tk.W)
+        tk.Label(page, text='$25').grid(row=2, column=2, sticky=tk.W)
 
         # Kayak
-        tk.Label(page, text='Kayak').grid(row=3, column=0, sticky=tk.W)
+        tk.Label(page, text='Kayak', font=('Arial', 14)).grid(row=3, column=0, sticky=tk.W)
         self.kayak_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.kayak_var, values=[0,1,2,3,4,5]).grid(row=3, column=1, sticky=tk.W)
-        tk.Label(page, text=self.kayak_var.get()).grid(row=3, column=2, sticky=tk.W)
+        tk.Label(page, text='$15').grid(row=3, column=2, sticky=tk.W)
 
         # Paddle Board
-        tk.Label(page, text='Paddle Board').grid(row=4, column=0, sticky=tk.W)
+        tk.Label(page, text='Paddle Board', font=('Arial', 14)).grid(row=4, column=0, sticky=tk.W)
         self.paddleboard_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.paddleboard_var, values=[0,1,2,3,4,5]).grid(row=4, column=1, sticky=tk.W)
-        tk.Label(page, text=self.paddleboard_var.get()).grid(row=4, column=2, sticky=tk.W)
+        tk.Label(page, text='$20').grid(row=4, column=2, sticky=tk.W)
 
-        tk.Label(page, text='Accessories').grid(row=5, column=0, sticky=tk.W)
+        tk.Label(page, text='Accessories', font=('Arial', 14, 'bold')).grid(row=5, column=0, sticky=tk.W)
 
         # PFD
-        tk.Label(page, text='PFD').grid(row=6, column=0, sticky=tk.W)
+        tk.Label(page, text='PFD', font=('Arial', 14)).grid(row=6, column=0, sticky=tk.W)
         self.pfd_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.pfd_var, values=[0,1,2,3,4,5]).grid(row=6, column=1, sticky=tk.W)
-        tk.Label(page, text=self.pfd_var.get()).grid(row=6, column=2, sticky=tk.W)
+        tk.Label(page, text='$5').grid(row=6, column=2, sticky=tk.W)
 
         # Paddles
-        tk.Label(page, text='Paddles').grid(row=7, column=0, sticky=tk.W)
+        tk.Label(page, text='Paddles', font=('Arial', 14)).grid(row=7, column=0, sticky=tk.W)
         self.paddles_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.paddles_var, values=[0,1,2,3,4,5]).grid(row=7, column=1, sticky=tk.W)
-        tk.Label(page, text=self.paddles_var.get()).grid(row=7, column=2, sticky=tk.W)
+        tk.Label(page, text='$5').grid(row=7, column=2, sticky=tk.W)
 
         # Dry Bag
-        tk.Label(page, text='Dry Bag').grid(row=8, column=0, sticky=tk.W)
+        tk.Label(page, text='Dry Bag', font=('Arial', 14)).grid(row=8, column=0, sticky=tk.W)
         self.drybag_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.drybag_var, values=[0,1,2,3,4,5]).grid(row=8, column=1, sticky=tk.W)
-        tk.Label(page, text=self.drybag_var.get()).grid(row=8, column=2, sticky=tk.W)
+        tk.Label(page, text='$5').grid(row=8, column=2, sticky=tk.W)
 
         # Dry Pack
-        tk.Label(page, text='Dry Pack').grid(row=9, column=0, sticky=tk.W)
+        tk.Label(page, text='Dry Pack', font=('Arial', 14)).grid(row=9, column=0, sticky=tk.W)
         self.drypack_var = tk.IntVar()
         ttk.Combobox(page, textvariable=self.drypack_var, values=[0,1,2,3,4,5]).grid(row=9, column=1, sticky=tk.W)
-        tk.Label(page, text=self.drypack_var.get()).grid(row=9, column=2, sticky=tk.W)
+        tk.Label(page, text='$10').grid(row=9, column=2, sticky=tk.W)
 
-        tk.Button(page, text='Calculate', command=self.calculateLandCost(self.tent_var.get(), self.sleepingbag_var.get(), self.backpack_var.get(), self.twoburnerstove_var.get(), self.lantern_var.get(), self.cookpotset_var.get(), self.watercooler_var.get(), self.hammock_var.get())).grid(row=11, column=2)
+        
 
         
 
     def transactionSummaryPage(self):
         page = tk.Frame(self.master)
         page.grid()
-        tk.Label(page, text='Transaction Summary').grid(row=0)
+        tk.Label(page, text='Transaction Summary', font=('Arial', 14, 'bold')).grid(row=0)
         tk.Button(page, text='Back', command=self.goto_page2).grid(row=11)
+        tk.Label(page, text=f'Start Date: {self.startDate}').grid(row=11, column=1)
+        tk.Label(page, text=f'End Date: {self.endDate}').grid(row=11, column=2)
+        tk.Label(page, text=self.calcTotal)
         # tk.Button(page, text='Submit', command=self.goto_page4).grid(row=11, column=1)
 
     def goto_page1(self):
@@ -253,10 +268,6 @@ class RecreationCenterApplication:
             self.transactionSummaryPage()
         
         
-
-
-
-
 
 
 if __name__ == '__main__':
